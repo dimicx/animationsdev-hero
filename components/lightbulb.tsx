fc4a407316db358a2adee33f0480cbf970240d04
dima <dumitrucepoi03@gmail.com>
2025-11-25 21:57:50 +0000

    timings

--- components/lightbulb.tsx
+++ components/lightbulb.tsx
@@ -43,7 +43,7 @@ const bulbVariants: Variants = {
     },
   },
   idle: {
-    opacity: [1, 0.2, 0.8, 0.2, 0.2, 1],
+    opacity: [1, 0.2, 1, 0.2, 0.2, 1],
     transition: {
       duration: 0.7,
       times: [0, 0.05, 0.1, 0.2, 0.5, 0.7],
@@ -67,10 +67,10 @@ const stemVariants: Variants = {
     },
   },
   idle: {
-    opacity: [1, 0.3, 0.3, 1],
+    opacity: [1, 0.3, 1, 0.3, 0.3, 1],
     transition: {
       duration: 0.7,
-      times: [0, 0.2, 0.5, 0.7],
+      times: [0, 0.05, 0.1, 0.2, 0.5, 0.7],
       delay: REPEAT_DELAY,
       repeat: Infinity,
       repeatType: "loop",
