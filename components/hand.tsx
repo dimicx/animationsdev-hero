053f0f7aece9e905c1bedcbbb53b517fd8e15c95
dima <dumitrucepoi03@gmail.com>
2025-12-08 09:10:16 +0000

    ray delays

--- components/hand.tsx
+++ components/hand.tsx
@@ -52,7 +52,7 @@ const rayVariants: Variants = {
       pathLength: [pathLength, 1, 1, 0.01, 0.01, 1],
       strokeOpacity: [0, 0.5, 0, 0, 0.5, 0.5],
       transition: {
-        delay: 0.3 + i * 0.05,
+        delay: 0.3 + (i === 1 ? 0 : 0.05),
         duration: 0.65,
         times: [0, 0, 0, 0.1, 0.1, 0.4],
       },
@@ -64,7 +64,7 @@ const rayVariants: Variants = {
       pathLength: [pathLength, pathLength, 0.01, 0.01, pathLength],
       strokeOpacity: [0.5, 0, 0, 0.5, 0.5],
       transition: {
-        delay: REPEAT_DELAY / 2 + (0.2 + i * 0.05),
+        delay: REPEAT_DELAY / 2 + 0.2,
         duration: 0.65,
         times: [0, 0, 0.1, 0.2, 0.4],
         repeat: Infinity,
